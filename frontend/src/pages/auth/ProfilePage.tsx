74bdee3365461074e117c4e1fa458d18573e2747
Jonnhy <104951133+qJona65p@users.noreply.github.com>
2026-07-18 13:49:16 -0600

    Added Search and Pagination to Courses

--- frontend/src/pages/auth/ProfilePage.tsx
+++ frontend/src/pages/auth/ProfilePage.tsx
@@ -1,5 +1,10 @@
 export default function ProfilePage(){
     return (
-        <p>Profile</p>
+        <div className="flex min-h-screen bg-slate-100 dark:bg-slate-900">
+            <div className="py-20 mx-auto px-8 max-w-7xl">
+                <img src="/profile.jpg" alt="Profile image" className="rounded-full" />
+                <p>Profile</p>
+            </div>
+        </div>
     );
 }
--- frontend/src/pages/courses/CoursesPage.tsx
+++ frontend/src/pages/courses/CoursesPage.tsx
@@ -1,5 +1,7 @@
+import { useMemo, useState } from "react";
 import CourseCard from "../../components/CourseCard";
 import type { CourseCardProps } from "../../lib/types";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function CoursesPage() {
     const courses: CourseCardProps[] = [
@@ -36,10 +38,80 @@ export default function CoursesPage() {
         { Id: 31, Name: "", Description: "", CategoryName: "Programming"},
         { Id: 32, Name: "", Description: "", CategoryName: "PersonalDevelopment"}
     ];
-    return (
+
+    // Search functions
+    const [searchTerm, setSearchTerm] = useState("");
+    const filteredCourses = useMemo(() => {
+        const search = searchTerm.trim().toLowerCase();
+        if (!search) return courses;
+        return courses.filter(
+            (course) => course.Name.toLowerCase().includes(search) || course.CategoryName.toLowerCase().includes(search)
+        )
+    }, [courses, searchTerm]);
+
+    // Pagination
+    const itemsPerPage = 12;
+    const totalPages = Math.ceil(filteredCourses.length / itemsPerPage);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const firstIndex = (currentPage - 1) * itemsPerPage;
+    const paginatedCourses = filteredCourses.slice(firstIndex, firstIndex+itemsPerPage);
+
+    // Pagination handlers
+    const handlePrevious = () => {setCurrentPage((prev) => Math.max(prev - 1, 1))};
+    const handleNext = () => {setCurrentPage((prev) => Math.min(prev + 1, totalPages))};
+    const goToPage = (pagenum: number) => {setCurrentPage(Math.min(Math.max(pagenum, 1), totalPages))};
+
+    useMemo(() => {
+        setCurrentPage(1);
+    }, [searchTerm]);
+
+    return ( 
         <div className="flex min-h-screen bg-slate-100 dark:bg-slate-900">
-            <div className="grid gap-8 md:grid-cols-4 py-20 mx-auto px-8">
-                { courses.map(c => <CourseCard Id={c.Id} Name={c.Name} Description={c.Description} CategoryName={c.CategoryName}/>) }
+            <div className="flex-grow w-full max-w-7xl mx-auto py-10 px-10 ">
+                {/* Title & SearchBar */}
+                <div>
+                    <h1 className="text-4xl font-extrabold leading-tight dark:text-white">All courses</h1>
+                    <input type="text" className="form-input mt-3" placeholder="Search courses"
+                        value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)}
+                    />
+                </div>
+
+                {/* Course Grid */}
+                {paginatedCourses.length > 0 ? (
+                    <div className="grid gap-8 md:grid-cols-3 py-8 px-8">
+                        { paginatedCourses.map(c => <CourseCard Id={c.Id} Name={c.Name} Description={c.Description} CategoryName={c.CategoryName}/>) }
+                    </div>
+                ): (
+                    <p className="mt-8 text-center text-slate-500 dark:text-slate-400">
+                        No course matches the search.
+                    </p>
+                )}
+                {/* Pagination Controls */}
+                {totalPages > 1 && (
+                    <div className="flex w-full mx-auto justify-center">
+                        <button onClick={handlePrevious} disabled={currentPage === 1}
+                            className="btn-primary rounded-full mx-2 px-2.5">
+                            <ChevronLeft />
+                        </button>
+                        {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+                            <button
+                                key={page}
+                                onClick={() => goToPage(page)}
+                                className={`rounded-full px-4.5 py-0 text-sm font-medium border-none mx-1 ${page === currentPage
+                                        ? "btn-primary"
+                                        : "hover:bg-slate-200 dark:border-slate-600 dark:text-white dark:hover:bg-slate-700"
+                                    }`}
+                            >
+                                {page}
+                            </button>
+                        ))}
+                        <button onClick={handleNext} disabled={currentPage === totalPages}
+                            className="btn-primary rounded-full mx-2 px-2.5">
+                            <ChevronRight />
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
     );
