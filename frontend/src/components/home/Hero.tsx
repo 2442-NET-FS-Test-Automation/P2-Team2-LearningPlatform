cab02de350395bc97e9e1a841c555e3e8472ddde
Jonnhy <104951133+qJona65p@users.noreply.github.com>
2026-07-21 22:15:19 -0600

    Tweaked CSS Styling

--- frontend/src/components/home/Hero.tsx
+++ frontend/src/components/home/Hero.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Hero() {
     return (
         <section className="section-light">
-            <div className="mx-auto flex min-h-[70vh] max-w-7xl flex-col items-center justify-center px-8 text-center">
+            <div className="mx-auto flex min-h-[95vh] max-w-7xl flex-col items-center justify-center px-8 text-center">
                 <h1 className="text-6xl font-extrabold leading-tight">
                     Learn without
                     <span className="text-blue-600 dark:text-blue-400">
