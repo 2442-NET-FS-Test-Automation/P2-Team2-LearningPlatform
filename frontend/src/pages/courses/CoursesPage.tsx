fc0af937f91acb3e102efd375859b289bee98adf
Jonnhy <104951133+qJona65p@users.noreply.github.com>
2026-07-24 09:28:57 -0600

    Reupload courses page

---
+++ frontend/src/pages/courses/CoursesPage.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useMemo, useState } from "react";
+import CourseCard from "../../components/CourseCard";
+import PaginationControls from "../../components/layout/PaginationControls";
+import type { CourseCompact } from "../../lib/types";
+import { getEnabledCourses } from "../../api/coursesRequests";
+
+export default function CoursesPage() {
+    const [courses, setCourses] = useState<CourseCompact[]>([]);
+
+    // Search functions
+    const [searchTerm, setSearchTerm] = useState("");
+    const filteredCourses = useMemo(() => {
+        const search = searchTerm.trim().toLowerCase();
+        if (!search) return courses;
+        return courses.filter(
+            (course) => course.name.toLowerCase().includes(search) || course.category.toLowerCase().includes(search)
+        )
+    }, [courses, searchTerm]);
+    
+    // Get Courses from the API
+    const [totalCourses, setTotalCourses] = useState(0)
+    useEffect(() => {
+        getEnabledCourses().then((res) => {
+            setCourses(res.items);
+            setTotalCourses(res.totalItems);
+        })
+    }, [])
+    
+    // Pagination
+    const [itemsPerPage, setItemsPerPage] = useState(6);
+    const totalPages = Math.ceil(totalCourses / itemsPerPage);
+    
+    const [currentPage, setCurrentPage] = useState(1);
+    const firstIndex = (currentPage - 1) * itemsPerPage;
+    const paginatedCourses = filteredCourses.slice(firstIndex, firstIndex+itemsPerPage);
+    
+    // Pagination handlers
+    const handlePrevious = () => {setCurrentPage((prev) => Math.max(prev - 1, 1))};
+    const handleNext = () => {setCurrentPage((prev) => Math.min(prev + 1, totalPages))};
+    const goToPage = (pagenum: number) => {setCurrentPage(Math.min(Math.max(pagenum, 1), totalPages))};
+
+    useMemo(() => {
+        setCurrentPage(1);
+    }, [searchTerm, itemsPerPage]);
+
+    return (
+        <div className="flex min-h-screen bg-slate-100 dark:bg-slate-900">
+            <div className="flex-grow w-full container-page py-10 ">
+                {/* Title & SearchBar */}
+                <div>
+                    <h1 className="text-4xl font-extrabold leading-tight">All courses</h1>
+                    <input type="text" className="form-input mt-3" placeholder="Search courses"
+                        value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)}
+                    />
+                </div>
+
+                {/* Course Grid */}
+                {paginatedCourses.length > 0 ? (
+                    <div className="card-grid py-8 px-8">
+                        { paginatedCourses.map(c => <CourseCard key={c.id} Id={c.id} Name={c.name} Description={c.description} CategoryName={c.category}/>) }
+                    </div>
+                ): (
+                    <p className="mt-8 text-center text-slate-500 dark:text-slate-400">
+                        No course matches the search.
+                    </p>
+                )}
+
+                <PaginationControls
+                    totalPages={totalPages} 
+                    currentPage={currentPage} 
+                    goToPage={goToPage} 
+                    handlePrevious={handlePrevious} 
+                    handleNext={handleNext} 
+                    setItemsPerPage={setItemsPerPage} 
+                />
+            </div>
+        </div>
+    );
+}
