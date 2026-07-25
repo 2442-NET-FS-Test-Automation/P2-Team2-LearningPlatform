2fb2251b964d12d44d4cbfb19d09f09ff1d1a833
Jonnhy <104951133+qJona65p@users.noreply.github.com>
2026-07-25 00:28:25 -0600

    CourseCard Prettier, CoursePage better

--- frontend/src/components/CourseCard.tsx
+++ frontend/src/components/CourseCard.tsx
@@ -14,13 +14,13 @@ export default function CourseCard({
     CategoryName
 }: CourseCardProps) {
     return (
-        <Link to={"/courses/" + Id} className="no-underline">
-            <div className="card p-3">
-                <img src={"/course_img/" + CategoryName + ".jpg"} alt={CategoryName + " image"} className="rounded-xl aspect-video"/>
-                <div className="p-4">
+        <Link to={"/courses/" + Id} className="no-underline h-full">
+            <div className="card p-3 h-full flex flex-col">
+                <img src={"/course_img/" + CategoryName + ".jpg"} alt={CategoryName + " image"} className="rounded-xl aspect-video object-cover"/>
+                <div className="p-4 flex-1 flex flex-col">
                     <h3 className="text-xl font-semibold">{Name}</h3>
-                    <p className="mt-3 text-muted">{Description}</p>
-                    <p className="mt-8 text-muted">{CategoryName}</p>
+                    <p className="mt-3 mb-6 text-muted">{Description}</p>
+                    <p className="text-muted mt-auto">{CategoryName}</p>
                 </div>
             </div>
         </Link>
