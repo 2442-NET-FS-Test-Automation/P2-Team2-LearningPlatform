799a08d8695731253d0d85212edd0c2d1cc6a19c
Jonnhy <104951133+qJona65p@users.noreply.github.com>
2026-08-03 22:03:45 -0600

    cypress component testing setup

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from "cypress";
+import registerCodeCoverage from "@cypress/code-coverage/task";
+import getCompareSnapshotsPlugin from "cypress-image-diff-js/plugin";
 
 export default defineConfig({
+    allowCypressEnv: false,
     e2e: {
         baseUrl: "http://localhost:5173",
-        supportFile: "cypress/support/e2e.js"
-    }
+        supportFile: "cypress/support/e2e.js",
+        setupNodeEvents(on, config) {
+            on("task", {
+                Log(message) {
+                    console.log(`[spec] ${message}`);
+                    return null;
+                }
+            });
+
+            registerCodeCoverage(on, config);
+            getCompareSnapshotsPlugin(on, config);
+
+            return config;
+        }
+    },
+    component: {
+        devServer: {
+            framework: "react",
+            bundler: "vite",
+        }
+    },
 })
