d8e46de59340a5a7cc4481b699aa67f2f49ce3f2
Jonnhy <104951133+qJona65p@users.noreply.github.com>
2026-08-04 22:01:29 -0600

    Add mount function to Register Page

--- frontend/cypress/component/authN/Register.cy.tsx
+++ frontend/cypress/component/authN/Register.cy.tsx
@@ -1,7 +1,18 @@
 import { MemoryRouter } from "react-router-dom";
+import { mount } from "cypress/react";
+
+import { AuthProvider } from "../../../src/ctx/AuthCtx";
+import RegisterPage from "../../../src/pages/auth/RegisterPage"
+
 
 describe("Register Page Tests", () => {
     it("TC-AuthN-01 - birth date eligibility", () => {
-        
+        mount(
+            <MemoryRouter>
+                <AuthProvider>
+                    <RegisterPage />
+                </AuthProvider>
+            </MemoryRouter>
+        );
     });
 });
