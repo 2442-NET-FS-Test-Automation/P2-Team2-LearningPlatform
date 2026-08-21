993cf737722910080126cffc14d59f7f0c34e679
Oscar Alfredo Martinez Gutierrez <martinezgtz.oscar.alfredo@gmail.com>
2026-08-20 22:36:49 -0600

    Last fixes

--- frontend/cypress/support/component.js
+++
@@ -1,9 +0,0 @@
-import "./commands";
-import "@cypress/code-coverage/support";
-
-import "../../src/index.css";
-
-import {mount} from "cypress/react";
-import { interfaces } from "mocha";
-
-Cypress.Commands.add("mount", mount);
