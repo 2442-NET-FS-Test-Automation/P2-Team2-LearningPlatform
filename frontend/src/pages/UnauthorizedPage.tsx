b9543343a78094e98f55a9ff5f1cef71c6fcdcf1
Jonnhy <104951133+qJona65p@users.noreply.github.com>
2026-08-21 11:47:16 -0600

    removed Marcille

--- frontend/src/pages/UnauthorizedPage.tsx
+++ frontend/src/pages/UnauthorizedPage.tsx
@@ -1,7 +1,7 @@
 export default function UnauthorizedPage() {
     return (
         <div className="flex h-[95vh] items-center justify-center">
-            <img src="/Unauthorized.png" alt="Unauthorized image" className="rounded-xl" width={150} height={150} />
+            {/* <img src="/Unauthorized.png" alt="Unauthorized image" className="rounded-xl" width={150} height={150} /> */}
             <h1 className="mx-10 text-3xl font-bold">401 | Unauthorized</h1>
         </div>
     );
